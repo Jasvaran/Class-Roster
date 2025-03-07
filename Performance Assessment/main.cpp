6a07330947d6e675209523cd592ceca401494dc7
Jasvaran Mann <mannjesse355@gmail.com>
2025-03-06 20:02:05 -0800

    Initial Commit

---
+++ Performance Assessment/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Performance Assessment
+//
+//  Created by Jasvaran Mann on 3/6/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
